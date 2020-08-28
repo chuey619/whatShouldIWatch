a46986db84d61fcf0b8ea4060bdde4d443303420
jolean lim <joleanflima@outlook.com>
2020-08-28 13:18:34 -0400

    auth backend

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -6,3 +6,39 @@ const usersController = require('../controllers/users-controller');
 
 authRouter.post('/reigster', usersController.create);
 
+authRouter.post('/login', passport.authenticate('local', {
+    successRedirect: '/home',
+    failureRedirect: '/home',
+    failureFlash: true,
+})
+);
+
+authRouter.get('/verify', (req, res) => {
+    if(req.user) return res.status(200).json({
+        message: 'ok',
+        auth: true,
+        data: {
+            user: req.user,
+        }
+    });
+    else return res.status(400).json({
+        message: 'Retry Login',
+        auth: false,
+        data: {
+            user: null,
+        }
+    });
+});
+
+authRouter.get('/logout', (req, res) => {
+    req.logout();
+    res.json({
+        message: 'logged out',
+        auth: false,
+        data: {
+            user: null,
+        }
+    })
+});
+
+module.exports = authRouter;
