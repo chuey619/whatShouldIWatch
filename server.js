74894c1130ca96a4671c89a7eb4d1f9b9c7e0e62
chueyi619 <chuey.israel@gmail.com>
2020-08-29 11:39:49 -0700

    added search functionality but we will have to smooth some stuff out

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(methodOverride("_method"));
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(cookieParser());
 
 app.use(
