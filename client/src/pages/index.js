61755a572d1dbe67556666344a5370786fdea971
chueyi619 <chuey.israel@gmail.com>
2020-08-30 08:35:23 -0700

    mpre conflicts

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,3 +2,4 @@ export { default as Home } from "./Home";
 export { default as Register } from "./Register";
 export { default as Login } from "./Login";
 export { default as About } from "./About";
+export { default as Search } from "./Search";
