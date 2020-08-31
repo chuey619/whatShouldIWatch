ed95adb88855ef4a21dfff943511178347a7a9b6
Charlotte Resnick <charlotte.resnick@gmail.com>
2020-08-31 07:45:30 -0700

    home page in progress

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
 import { Navbar, SubscriptionsModal } from "../components/";
 import Footer from "../components/Footer";
-import { useDisclosure } from "@chakra-ui/core";
+import {
+  useDisclosure,
+  Box,
+  Flex,
+  Image,
+  SimpleGrid,
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+} from "@chakra-ui/core";
 import { useParams } from "react-router-dom";
 import useQuery from "../hooks/useQuery";
 
@@ -11,7 +22,71 @@ function Home() {
   return (
     <>
       <Navbar />
-      <div>Home</div>
+      <SimpleGrid columns={2} spacingX="0" spacingY="20px" bg="black">
+        <Box
+          bg="black"
+          height="80vh"
+          width="30vw"
+          overflow="scroll"
+          minW="222px"
+        >
+          <Image
+            borderRadius="100px"
+            boxShadow="1px 3px 32px #4a3853"
+            src="/assets/netflix-circle.png"
+            width="180px"
+            height="180px"
+            mt="20px"
+            ml="20px"
+          ></Image>
+          <Image
+            borderRadius="100px"
+            boxShadow="1px 3px 32px #4a3853"
+            src="/assets/hulu-circle.jpg"
+            width="180px"
+            height="180px"
+            mt="20px"
+            ml="20px"
+          ></Image>
+          <Image
+            borderRadius="100px"
+            boxShadow="1px 3px 32px #4a3853"
+            src="/assets/disneyplus-circle.jpg"
+            width="180px"
+            height="180px"
+            mt="20px"
+            mb="20px"
+            ml="20px"
+          ></Image>
+        </Box>
+        <Box
+          bg="black"
+          height="80vh"
+          width="70vw"
+          display="flex"
+          flexDirection="column"
+          alignItems="left"
+          pl="20px"
+          pr="30px"
+          pt="30vh"
+        >
+          <Heading color="white">Browse movies and shows in one click.</Heading>
+          <p>Search from six services and save your favorites for later!</p>
+          <form>
+            <FormControl>
+              <Input
+                type="search"
+                width="30vw"
+                placeholder="What would you like to watch?"
+                mt="20px"
+              />
+            </FormControl>
+            <Button width="20vw" mt={4} type="submit" variantColor="purple">
+              Search
+            </Button>
+          </form>
+        </Box>
+      </SimpleGrid>
       <SubscriptionsModal isOpen={isOpen} onOpen={onOpen} onClose={onClose} />
       <Footer />
     </>
