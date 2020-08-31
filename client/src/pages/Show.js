8b9bed07548c3cacd6b8183b1b6c943e5c396053
chueyi619 <chuey.israel@gmail.com>
2020-08-31 13:35:03 -0700

    search to show flushed out but its ugly

--- client/src/pages/Show.js
+++ client/src/pages/Show.js
@@ -2,8 +2,41 @@ import React from "react";
 class Show extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentMedia: {},
+      locations: [],
+    };
+  }
+  componentDidMount() {
+    fetch(`/api/media/${this.props.match.params.id}`)
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          currentMedia: json.data.collection,
+          locations: json.data.collection.locations,
+        });
+      });
   }
   render() {
-    return "jhello";
+    return (
+      <div>
+        {this.state.currentMedia !== {} ? (
+          <div>
+            <h1>{this.state.currentMedia.name}</h1>
+            {console.log(this.state.currentMedia.locations)}
+            <ul>
+              {this.state.locations &&
+                this.state.locations.map((location) => {
+                  return <a href={location.url}>{location.display_name}</a>;
+                })}
+            </ul>
+          </div>
+        ) : (
+          <p>loading</p>
+        )}
+      </div>
+    );
   }
 }
+
+export default Show;
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -4,3 +4,4 @@ export { default as Login } from "./Login";
 export { default as About } from "./About";
 export { default as Search } from "./search";
 export { default as Results } from "./Results";
+export { default as Show } from "./Show";
