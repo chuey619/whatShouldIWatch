cebfdddd67269a21c57b2939b4feb66b6d47fe0e
Charlotte Resnick <charlotte.resnick@gmail.com>
2020-09-01 09:24:15 -0700

    removed unused imports

--- client/src/hooks/useUser.js
+++ client/src/hooks/useUser.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import { useState, useCallback } from "react";
 
 const useUser = () => {
   const [user, setUser] = useState({ user: {} });
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Redirect, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import {
   Flex,
   Box,
@@ -9,7 +9,6 @@ import {
   Input,
   Button,
 } from "@chakra-ui/core";
-import axios from "axios";
 
 const Register = (props) => {
   const [user, setUser] = useState({ username: "", email: "", password: "" });
