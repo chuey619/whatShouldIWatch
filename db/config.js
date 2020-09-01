a4bd422b89a63f361aa188188fe2a190145effcb
chueyi619 <chuey.israel@gmail.com>
2020-09-01 10:40:49 -0700

    i dont want to push this

--- db/config.js
+++ db/config.js
@@ -1,22 +1,24 @@
-
-
+require("dotenv").config();
 const options = {
-    query: (e) => {
-        console.log(e.query);
-    }
-}
+  query: (e) => {
+    console.log(e.query);
+  },
+};
 
-const pgp = require('pg-promise')(options);
+const pgp = require("pg-promise")(options);
 
 function setDatabase() {
-    if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
-        return pgp({
-            database: 'streaming_dev',
-            port: 5432,
-            host: 'localhost',
-        })
-     } else if (process.env.NODE_ENV === 'production') {
-        return pgp(process.env.DATABASE_URL);
-        }
-    }
-const db = setDatabase;
+  if (process.env.NODE_ENV === "development" || !process.env.NODE_ENV) {
+    return pgp({
+      database: "what_should_i_watch",
+      port: 5432,
+      host: "localhost",
+      user: "postgres",
+      password: process.env.PASSWORD,
+    });
+  } else if (process.env.NODE_ENV === "production") {
+    return pgp(process.env.DATABASE_URL);
+  }
+}
+
+module.exports = setDatabase();
